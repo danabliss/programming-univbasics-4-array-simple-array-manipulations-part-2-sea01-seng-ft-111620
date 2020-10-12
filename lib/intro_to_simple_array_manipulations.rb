d740b24bbb53fbf9829995ac7c41b84364c356f0
Dana Jackson <danajackson2@gmail.com>
2020-10-12 19:00:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,14 @@ def using_uniq(array)
   array.uniq
 end  
 
-def using_flatten(array, string)
-  array.delete(string)
+def using_flatten(array)
+  array.flatten
 end  
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at
+
+end
